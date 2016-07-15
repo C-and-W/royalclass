c93e30eacea6336b06711fb4b20af2b21b53aab4
jacqhernandez <jm.hernandez96@yahoo.com>
2016-07-15 07:19:01 -0200

    metatags

--- sitemap_www.rb
+++ sitemap_www.rb
@@ -5,14 +5,12 @@ SitemapGenerator::Sitemap.default_host = 'http://www.royalclassgroup.com'
 SitemapGenerator::Sitemap.create do
   add '/', :changefreq => 'weekly', :priority => 1.00
   add '/company', :changefreq => 'weekly', :priority => 0.80
-  add '/company/', :changefreq => 'weekly', :priority => 0.80
   add '/rcp', :changefreq => 'weekly', :priority => 0.80
   add '/rct', :changefreq => 'weekly', :priority => 0.80
   add '/rcd', :changefreq => 'weekly', :priority => 0.80
   add '/jpgv', :changefreq => 'weekly', :priority => 0.80
   add '/rcf', :changefreq => 'weekly', :priority => 0.80
   add '/contact', :changefreq => 'weekly', :priority => 0.80
-  add '/contact/', :changefreq => 'weekly', :priority => 0.80
   add '/rcp/gallery', :changefreq => 'weekly', :priority => 0.64
   add '/rct/gallery', :changefreq => 'weekly', :priority => 0.64
   add '/rcd/gallery', :changefreq => 'weekly', :priority => 0.64
